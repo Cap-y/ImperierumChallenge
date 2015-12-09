2709413efd55e946212e7a2f83efe015e698ebbb
Jesper <jesper@jesper.se>
2015-12-09 14:27:49 +0100

    fixade user.php

---
+++ _old_DB/2015_12_01_085747_create_questions_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('challenge_id')->unsigned();
+            $table->foreign('challenge_id')->references('id')->on('challenges');
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions_options');
+    }
+}
