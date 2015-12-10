719b730f84d51e544474532f2e9dc8664d9a4932
capy <carl_mard@hotmail.com>
2015-12-10 11:37:09 +0100

    Questions and challenges

--- laravel/app/Option.php
+++ laravel/app/Option.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 class Option extends Model
 {
     protected $table = 'options';
+
+    public function questions(){
+    	return $this->belongsToMany('App\Question', 'questions_options');
+    }
 }
