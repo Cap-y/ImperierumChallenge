ebefb6820fca19e94a31c1b9c7743c39ee4a8bef
capy <carl_mard@hotmail.com>
2015-12-18 13:20:03 +0100

    User.php conflict fix

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -16,10 +16,8 @@ class User extends Model implements AuthenticatableContract,
 {
     use Authenticatable, Authorizable, CanResetPassword;
 
-<<<<<<< HEAD
-=======
     protected $fillable = ['firstname','lastname','alias','city'];
->>>>>>> 32a113bd541bf428d50dcc2e5582edfe0d3cdff4
+
     /**
      * The database table used by the model.
      *
@@ -32,19 +30,17 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-<<<<<<< HEAD
+
     protected $fillable = ['name', 'email', 'password'];
 
-=======
->>>>>>> 32a113bd541bf428d50dcc2e5582edfe0d3cdff4
+
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
-<<<<<<< HEAD
-=======
+
 
     public function challenges(){
         return $this->belongsToMany('App\Challenge', 'users_challenges');
@@ -56,5 +52,4 @@ class User extends Model implements AuthenticatableContract,
         return $this->belongsToMany('App\Connection', 'users_connections');
     }
 
->>>>>>> 32a113bd541bf428d50dcc2e5582edfe0d3cdff4
 }
